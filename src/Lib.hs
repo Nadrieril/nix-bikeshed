afe0f4094c1ad8f23a0aeb283a3c67840ab172b3
Léo Gaspard <leo@gaspard.io>
2018-04-05 13:14:33 +0200

    wip

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (indent) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Nix.Expr
+import Nix.Parser
+
+doTheThing :: IO ()
+doTheThing = putStrLn $ indent $ parse "/proc/self/fd/1"
+
+parse :: FilePath -> NExpr
+parse f = do
+    expr <- parseNixFile f
+    case expr of
+        Success a -> a
+        Failure e -> error $ show e
+
+indent :: NExpr -> String
+indent a = "someFunc"
